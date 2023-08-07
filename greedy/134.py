67f5e60aaf3f7915ac4699bdad395b2edc5ae356
sangbumlikeagod <oironese@naver.com>
2023-08-07 11:05:37 +0900

    8월 7일 134번 풀이 제출

---
+++ greedy/134.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        left_sum = 0
+        right_sum = 0
+        idx = 0 
+        size = len(gas)
+        for i in range(size):
+            right_sum += gas[i] - cost[i]
+            if right_sum < 0:
+                idx = i + 1
+                left_sum += right_sum
+                right_sum = 0
+        return idx if right_sum + left_sum >= 0 else -1

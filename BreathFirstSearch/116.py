7665ff12894bdaa972b7e92d076bf77bf722b93d
sangbumlikeagod <oironese@naver.com>
2023-08-12 23:07:43 +0900

    8월 12일 제출

--- BreathFirstSearch/116.py
+++ BreathFirstSearch/116.py
@@ -1,3 +1,25 @@
+
 class Solution:
     def connect(self, root):
+        from queue import Queue
+        lst = Queue()
+        if not root:
+            return None
+        def recur(root, idx):
+            
+            if root.left:
+                lst.put((root.left ,idx + 1))
+            if root.right: 
+                lst.put((root.right, idx + 1))
+            if not lst.empty():
+                next = lst.get()
+                if next[1] == idx:
+                    root.next = next[0]
+                else:
+                    root.next =  None
+                recur(next,  idx + 1)
+            else: return
+             
+        recur(root, 1)
+        return root
 

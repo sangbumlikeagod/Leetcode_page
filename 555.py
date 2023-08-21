b63d0432938492652be5f711b5119aa1e62989ca
SangbumKim <oironese@naver.com>
2023-08-22 08:51:13 +0900

    으악

---
+++ 555.py
@@ -0,0 +1,10 @@
+
+N, M = 10, 40
+
+omega = M % (N + 1) if M % (N + 1) else N + 1
+print(N - omega + 1 ,  M // (N + 1))
+print(N - omega + 1 >= M // (N + 1))
+
+# 33 - 7 인 상황 
+# 10 9 8 7 4개를 없애야 하는거 아님?
+# 

824406152bcfc6b180f9ed7a1365296e6643b4cb
sangbumlikeagod <oironese@naver.com>
2023-09-03 20:29:31 +0900

    62번 제출

---
+++ MATH/62.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        total = 1
+        memorization = [0] * (m + n + 1)
+        for i in range(1, (m + n + 1)):
+            total *= i
+            memorization[i] = total
+        

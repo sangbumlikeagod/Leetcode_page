0df56c8e330822aaaf0337f1454508f19dccd684
sangbumlikeagod <oironese@naver.com>
2023-11-08 23:11:46 +0900

    feat 2849.cpp

---
+++ C++/2849.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t)
+    {
+        int tmp;
+        if (sx > fx)
+        {
+            tmp = sx;
+            sx = fx;
+            fx = tmp;
+        }
+        if (sy > fy)
+        {
+            tmp = sy;
+            sy = fy;
+            fy = tmp;
+        }
+        int longestPath = fx - sx + fy - sy;
+        int shortt = min(fy - sy, fx - sx);
+        int shortestPath = longestPath - shortt;
+        // cout << shortt;
+        // cout << !(!shortestPath && (t == 1));
+
+        return (shortestPath <= t) && !(!shortestPath && (t == 1));
+    }
+};

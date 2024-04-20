12cd2affbbcddd3e04980e5822c98b978d01bd55
sangbumlikeagod <oironese@naver.com>
2024-04-20 14:57:09 +0900

    commit 84

--- C++/84.cpp
+++ C++/84.cpp
@@ -11,21 +11,26 @@ public:
         right[siz - 1] = siz - 1;
         for (int i = 1; i < siz; i++)
         {
+            right[i] = i;
+            left[i] = i;
             int hLeft = i;
             // 본인보다 크다면 왼쪽을 골라잉 이런걸 어떻게 빨리짜냐는거야
-            while (heights[i] <= heights[hLeft] && hLeft >= 0)
+            while (hLeft >= 0 && heights[i] <= heights[hLeft])
             {
                 left[i] = left[hLeft];
+                // cout << left[hLeft] - 1 << '\n';
                 hLeft = left[hLeft] - 1;
             }
         }
 
-        for (int i = siz - 1; i; i--)
+        for (int i = siz - 1; i >= 0; i--)
         {
             int hRight = i;
-            while (heights[i] <= heights[hRight] && hRight < siz)
+            // cout << '\t' << i << '\n';
+            while (hRight < siz && heights[i] <= heights[hRight])
             {
                 right[i] = right[hRight];
+                // cout << right[hRight] + 1 << '\n';
                 hRight = right[hRight] + 1;
             }
             ans = max(ans, heights[i] * (right[i] - left[i] + 1));

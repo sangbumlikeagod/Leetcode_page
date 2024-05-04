08f4c9e21e4b09f709ce67b243ae0bd35e6cd8f2
김상범 <oironese@naver.com>
2024-05-04 11:35:06 +0900

    240504: 881번 풀이

---
+++ 60062.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct node
+{
+    int val;
+    node *next;
+    node(int val): val(val) {};
+};
+
+
+int solution(int n, vector<int> weak, vector<int> dist) {
+
+    int answer = 0;
+    return answer;
+}

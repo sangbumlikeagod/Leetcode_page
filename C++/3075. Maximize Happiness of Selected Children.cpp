4c0de1dacee6709813e696c85c5f8f99770e0d42
김상범 <oironese@naver.com>
2024-05-09 23:46:00 +0900

    240509: 3075번 제출

---
+++ C++/3075. Maximize Happiness of Selected Children.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0);
+    ifstream cin(".txt", ios_base::in);
+    
+    
+}

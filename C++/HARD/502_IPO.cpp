147d1f4438d5924f09881fc69f24a9cd5ce50620
sangbumlikeagod <oironese@naver.com>
2024-06-15 23:52:42 +0900

    240615 : 제출

--- C++/HARD/502_IPO.cpp
+++ C++/HARD/502_IPO.cpp
@@ -3,5 +3,35 @@ class Solution
 public:
     int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
     {
+        vector<pair<int, int>> procap;
+        priority_queue<int> maxProfitStore;
+        for (int index = 0; index < capital.size(); index++)
+        {
+            procap.push_back({capital[index], profits[index]});
+        }
+        sort(procap.begin(), procap.end());
+        // int startCapital = w;
+        for (int index = 0; index < procap.size(); index++)
+        {
+            while (procap[index].first > w)
+            {
+                if (k == 0 || maxProfitStore.empty())
+                {
+                    return w;
+                }
+                w += maxProfitStore.top();
+                maxProfitStore.pop();
+                k--;
+            }
+
+            maxProfitStore.push(procap[index].second);
+        }
+        while (k > 0 && !maxProfitStore.empty())
+        {
+            w += maxProfitStore.top();
+            maxProfitStore.pop();
+            k--;
+        }
+        return w;
     }
 };

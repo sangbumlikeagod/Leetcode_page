c0c4b9d3613fa442d773474c3d3e647cd7dc9f7a
sangbumlikeagod <oironese@naver.com>
2024-06-17 10:42:09 +0900

    240617 제출

--- C++/HARD/330_Patch_Array.cpp
+++ C++/HARD/330_Patch_Array.cpp
@@ -1,7 +1,32 @@
 class Solution
 {
+    typedef long long ll;
+
 public:
     int minPatches(vector<int> &nums, int n)
     {
+        ll prefixSum = 1;
+        int ans = 0;
+        int index = 0;
+        while (index < nums.size() && prefixSum <= n)
+        {
+            int next = nums[index];
+            if (next <= prefixSum)
+            {
+                prefixSum += next;
+                index++;
+            }
+            else
+            {
+                prefixSum *= 2;
+                ans++;
+            }
+        }
+        while (prefixSum <= n)
+        {
+            ans++;
+            prefixSum *= 2;
+        }
+        return ans;
     }
 };

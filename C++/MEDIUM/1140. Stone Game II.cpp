8dfe8355ab78563f6c1a8686df974c0c4e1d3fbf
sangbumlikeagod <oironese@naver.com>
2024-08-21 21:01:56 +0900

    240821 제출

--- C++/MEDIUM/1140. Stone Game II.cpp
+++ C++/MEDIUM/1140. Stone Game II.cpp
@@ -1,7 +1,45 @@
 class Solution
 {
+    int dp[100][100] = {0};
+    vector<int> prefixSum = {0};
+    int size;
+
+private:
+    int DP(int x, int y)
+    {
+        if (x >= size)
+        {
+            return 0;
+        }
+        if (dp[x][y])
+        {
+            return dp[x][y];
+        }
+        if (x + 2 * y >= size)
+        {
+            dp[x][y] = prefixSum[size] - prefixSum[x];
+            return dp[x][y];
+        }
+
+        int mineTotal = prefixSum[size] - prefixSum[x];
+        int oppsTotal = 100000000;
+        for (int i = 1; i <= 2 * y; i++)
+        {
+            oppsTotal = min(oppsTotal, DP(x + i, max(i, y)));
+        }
+        dp[x][y] = mineTotal - oppsTotal;
+        return dp[x][y];
+    }
+
 public:
     int stoneGameII(vector<int> &piles)
     {
+        for (int pile : piles)
+        {
+            int before = prefixSum[prefixSum.size() - 1];
+            prefixSum.push_back(pile + before);
+        }
+        size = piles.size();
+        return DP(0, 1);
     }
 };

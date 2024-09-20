af423879b3191703db5507c7639570cf630482cb
sangbumlikeagod <oironese@naver.com>
2024-09-21 00:15:08 +0900

    240921 제출

--- C++/HARD/214. Shortest Palindrome.cpp
+++ C++/HARD/214. Shortest Palindrome.cpp
@@ -16,21 +16,18 @@ public:
         int K = s.length() * 2 - 1;
         vector<int> palin(K, 0);
 
-        // manacher.push_back('#');
         for (char c : s)
         {
             manacher.push_back(c);
             manacher.push_back('#');
         }
         manacher.pop_back();
-        // 모든 제대로된 문자인덱스들은 짝수
 
         for (int i = 0; i < K; i++)
         {
             if (i < right)
             {
-                // 왼쪽 끝은
-                palin[i] = min(palin[center - (i - center)], K - i - 1);
+                palin[i] = min(palin[center - (i - center)], right - i - 1);
             }
             int wing = palin[i];
             while (i - wing - 1 >= 0 && i + wing + 1 < K && manacher[i + wing + 1] == manacher[i - wing - 1])

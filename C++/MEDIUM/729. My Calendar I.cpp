f3cd9a3d9a6f40f0fc2c52a6b5ed0df620f2660c
sangbumlikeagod <oironese@naver.com>
2024-09-27 00:27:53 +0900

    240927 제출

--- C++/MEDIUM/729. My Calendar I.cpp
+++ C++/MEDIUM/729. My Calendar I.cpp
@@ -1,1 +1,28 @@
-#include <iostream>
+class MyCalendar
+{
+    vector<vector<int>> roots;
+
+public:
+    MyCalendar()
+    {
+    }
+
+    bool book(int start, int end)
+    {
+        for (vector<int> root : roots)
+        {
+            if ((start <= root[0] && end > root[0] && end < root[1]) || (start > root[0] && root[1] > start && root[1] < end) || (start >= root[0] && end <= root[1]) || (start <= root[0] && end >= root[1]))
+            {
+                return false;
+            }
+        }
+        roots.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */

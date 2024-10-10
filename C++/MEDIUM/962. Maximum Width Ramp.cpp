fd9e2dd89b185a9cf770a58bfcb4951ec8af21ac
sangbumlikeagod <oironese@naver.com>
2024-10-10 23:51:02 +0900

    241010 제출

---
+++ C++/MEDIUM/962. Maximum Width Ramp.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        vector<int> list {};
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            if (list.empty() || nums[list.back()] > num)
+            {
+                list.push_back(i);
+            }
+        }
+        int m = nums.size() - 1;
+        int n = list.size() - 1;
+        int answer = 0;
+        for (; m >= 0; m--)
+        {
+            while (!list.empty() && nums[m] >= nums[list[n]])
+            {
+                answer = max(answer, m - list[n]);
+                list.pop_back();
+                n--;
+            }
+        }
+        return answer;
+
+    }
+};

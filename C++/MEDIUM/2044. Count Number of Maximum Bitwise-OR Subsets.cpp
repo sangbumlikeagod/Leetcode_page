876a9de06c43f50f3c6c8ccf4037b5b351f4921f
sangbumlikeagod <oironese@naver.com>
2024-10-19 00:36:37 +0900

    241019 제출

--- C++/MEDIUM/2044. Count Number of Maximum Bitwise-OR Subsets.cpp
+++ C++/MEDIUM/2044. Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -1,7 +1,35 @@
 class Solution
 {
+    int answer;
+    int maxVal;
+    int na = 0;
+    void backTracking(int idx, int val, vector<int> &nums)
+    {
+        if (idx == na)
+        {
+            if (val == maxVal)
+            {
+                answer++;
+            }
+            return;
+        }
+
+        backTracking(idx + 1, val | nums[idx], nums);
+        backTracking(idx + 1, val, nums);
+    }
+
 public:
     int countMaxOrSubsets(vector<int> &nums)
     {
+        maxVal = 0;
+        answer = 0;
+        na = nums.size();
+        for (int num : nums)
+        {
+            maxVal |= num;
+        }
+
+        backTracking(0, 0, nums);
+        return answer;
     }
 };

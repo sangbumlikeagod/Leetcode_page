fe9fdfc7770862458d9453d6f31062bc6e0bb812
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2024-11-24 18:11:12 +0900

    Time: 95 ms (16.99%), Space: 119.8 MB (12.2%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        int answer = nums.size() + 1;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> q;
+
+        q.push({0, -1});
         long long prefixSum = 0;
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> queue; 
-        queue.push({0, -1});
+        int answer = nums.size() + 1;
+
         for (int i = 0; i < nums.size(); i++)
         {
             prefixSum += nums[i];
-            while(!queue.empty() && prefixSum - queue.top().first >= k)
+            while (!q.empty() && prefixSum - k >= q.top().first)
             {
-                answer = min(i - queue.top().second, answer);
-                queue.pop();
-            }
-            queue.push({prefixSum, i});
+                answer = min(answer,i - q.top().second);
+                q.pop();
+            }   
+            q.push({prefixSum, i});
         }
         return answer == nums.size() + 1 ? -1 : answer;
     }

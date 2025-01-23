ba10cd1856a553f3ef47c19ef2ca311fd2c39eef
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2025-01-23 23:21:21 +0900

    Time: 3 ms (55.91%), Space: 26.7 MB (39.9%) - LeetHub

---
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        vector<int> r(grid.size(), 0);
+        vector<int> c(grid[0].size(), 0);
+
+        int answer = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j])
+                {
+                    r[i]++;
+                    c[j]++;
+                    answer++;
+                }
+            }
+        }
+
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] && r[i] == 1 && c[j] == 1)
+                {
+                    answer--;
+                }
+            }
+        }
+        return answer;
+    }
+};

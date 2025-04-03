253aa47faf92ad9dfe384c27858afe20eae690d7
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2025-04-04 08:55:53 +0900

    Time: 0 ms (100%), Space: 90.9 MB (90.14%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -3,9 +3,6 @@ public:
     long long maximumTripletValue(vector<int>& nums) {
         long long answer = 0;
         int n = nums.size();
-        vector<int> stacck {nums[0]};
-
-        // 이제까지의 최대값, 그때까지의 차의 최대값, 그리고 그거에대한 항상 곱
         long long bigg = 0;
         long long bigg_difference = 0;
         for (int i = 0 ; i < n; i++)

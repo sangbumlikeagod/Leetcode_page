073afebbab36ccb2f27980054a96bf1bda245c5a
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2025-06-29 18:11:06 +0900

    Time: 36 ms (31.99%), Space: 51.4 MB (95.04%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,6 +1,5 @@
 class Solution {
     int MODULO = 1e9 + 7;
-    // 2의 2 ^ x승의 값
     vector<long long> powerOfTwo;
     
     void init() {
@@ -31,18 +30,12 @@ public:
         int n = nums.size();
         powerOfTwo.resize(20);
         init();
-        // for (int i = 0; i < 20; i++)
-        //     {
-        //         cout << powerOfTwo[i] << ' '; 
-        //     }
-        // cout <<'\n';
         sort(nums.begin(), nums.end());
         int answer = 0;
         for (int i = 0; i < n; i++)
             {
                 int possible = n - i - 1;
                 possible -= (int) (nums.end() - upper_bound(nums.begin(), nums.end(), target - nums[i]));
-                // cout << i << ' ' << nums.end() - upper_bound(nums.begin(), nums.end(), target - nums[i]) << ' '<< possible << '\n';
                 if (possible < 0) break;
                 answer += calc(possible);
                 answer %= MODULO;

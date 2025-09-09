bbe195742b16c16a8fd4145c8b81ef263f89e525
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2025-09-10 00:15:48 +0900

    Time: 5 ms (48.18%), Space: 41.7 MB (15.45%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.java
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.java
@@ -7,13 +7,10 @@ class Solution {
         int[] known = new int[n * 2];
         //
         int[] dKnown = new int[n * 2];
-        // 알게된애들 
-        int[] plus = new int[n * 2];
         // 까먹은 애들 
         int[] minus = new int[n * 2];
         current[0] = 1;
         dKnown[delay - 1] = 1;
-        plus[delay - 1] = 1;
         minus[forget - 1] = 1;
         for (int i = 1; i < n; i++)
         {
@@ -21,18 +18,12 @@ class Solution {
             known[i] = (int) (((long) known[i - 1] - (long) minus[i - 1] + (long) dKnown[i - 1]) % MODULO);
             if (known[i] != 0)
             {
-                plus[i] = known[i];
                 dKnown[i + delay - 1] = known[i];
             }
             minus[i + forget - 1] = known[i];
             current[i] = (int) (( (long) current[i - 1] + (long) known[i] - (long) minus[i - 1]) % MODULO);
         }
 
-        // System.out.println(Arrays.toString(current));
-        // System.out.println(Arrays.toString(dKnown));
-        // System.out.println(Arrays.toString(known));
-        // System.out.println(Arrays.toString(plus));
-        // System.out.println(Arrays.toString(minus));
         return (int) (((long) current[n - 1] + MODULO) % MODULO) ;
     }
 }

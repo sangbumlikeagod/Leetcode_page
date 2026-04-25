4db254075c0ab012c8c83cc7282c8004827bc1c3
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2026-04-25 17:47:20 +0900

    Time: 571 ms (24.39%), Space: 24.9 MB (85.37%) - LeetHub

--- 3464-maximize-the-distance-between-points-on-a-square/3464-maximize-the-distance-between-points-on-a-square.py
+++ 3464-maximize-the-distance-between-points-on-a-square/3464-maximize-the-distance-between-points-on-a-square.py
@@ -22,12 +22,7 @@ class Flatten:
 
     def circular_binary_search(self, q):
         cnt = 1
-        # 나온 값
         limit = len(self.flatten_list)
-        # idx = 0
-        
-        # 일단 얘보다 크면 안됨
-        first_query_result = -1
 
         possible_first_query = min(self.query(q, 0), limit - 1)
 
@@ -40,35 +35,16 @@ class Flatten:
                 qr = self.query(q, self.flatten_list[idx])
                 if qr == limit:
                     break
-                # if idx == 0:
-                #     first_query_result = qr
-
-                # print(idx, qr)
-                # print(qr, self.flatten_list[qr], lim_val)
 
                 if self.flatten_list[qr] > lim_val:
                     break
-                #     # 시작인덱스를 지좆대로로 했기 때문에 이런 에러에 걸린다.
-                #     last_query = -(self.n * 4 - self.flatten_list[qr])
-                #     # 마지막 인덱스 넘의 쿼리와 맞는 인덱스
-                #     last_query_val = self.query(q, last_query)
-                #     print("last_query:", last_query, "last_query_val", last_query_val)
-                #     if self.flatten_list[last_query_val] + q <= self.flatten_list[first_query_result]:
-                #         #얘로 바꾼다치고 이후엔 변환없음
-                #         cnt += 1
-                #     break
                 tcnt += 1
                 idx = qr
             cnt = max(cnt, tcnt)
-            
-            # print(f"interval: {q}, cnt: {cnt}")
         return cnt
 
 
     def query(self, q, x):
-        # 현재지점 x보다 q이상 큰점중 가장 작은 점을 구해라, 
-        # x인덱스
-
         l = 0
         r = len(self.flatten_list)
         query_val = x + q
@@ -92,7 +68,6 @@ class Solution:
         for point in points:
             flatten.append(point)
         flatten.flatten_list.sort()
-        # print(flatten.flatten_list)
 
         while l < r:
             m = (l + r) // 2 + (l + r) % 2

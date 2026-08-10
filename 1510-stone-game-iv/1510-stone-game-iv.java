1ef28e90d9b07c3d14a0d466dcffa8292a3a9c93
sangbumlikeagod <98077576+sangbumlikeagod@users.noreply.github.com>
2026-08-11 08:56:08 +0900

    Time: 49 ms (22.83%), Space: 43.2 MB (56.43%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -5,18 +5,19 @@ class Solution {
             // 1번이라도 0이 있으면
             // 내가 그만큼 빼버리면 되니까 
             if (DP[x] != -1) return DP[x]; 
-            int cantControl = 1;
             for (int i = arr.size() - 1; i >=0; i--)
             {
                 if (arr.get(i) > x)
                 {continue;}
                 if (arr.get(i) == x)
                 {return 1;}
-                // 얘가 1이 가능하면 안됨 
-                cantControl = (cantControl & dp(x - arr.get(i), arr)); 
-                // 0이 하나라도 있으면 넘길 수 있음
+                if (dp(x - arr.get(i), arr) == 0)
+                {
+                    DP[x] = 1;
+                    return 1;
+                }
             }
-            DP[x] = (cantControl ^ 1);
+            DP[x] = 0;
             return DP[x];
         }
     public boolean winnerSquareGame(int n) {
@@ -25,13 +26,9 @@ class Solution {
         Arrays.fill(DP, -1);
         
         // 1 is odd 0 is even 
-        // 
         DP[1] = 1;
-        // DP[2] = 0;
-        // DP[3] = 1;
         for (int i = 1; i * i <= n; i++)
-        {
-            // 한번도 안돼야함 
+        { 
             squares.add(i * i);
         }
         return dp(n, squares) == 1;
